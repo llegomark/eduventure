9f350a7c42bbe76c2ae68f2a01ecab4afb0d920c
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2022-11-22 23:46:15 +0800

    Firebase under the hood API improvements

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -26,6 +26,13 @@ export const serverSchema = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   OPENAI_API_KEY: z.string(),
+  FIREBASE_API_KEY: z.string(),
+  FIREBASE_APP_ID: z.string(),
+  FIREBASE_AUTH_DOMAIN: z.string(),
+  FIREBASE_PROJECT_ID: z.string(),
+  FIREBASE_STORAGE_BUCKET: z.string(),
+  FIREBASE_MESSAGING_SENDER_ID: z.string(),
+  FIREBASE_MEASUREMENT_ID: z.string(),
 });
 
 /**

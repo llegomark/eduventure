5ef38d1dd6dfcfc17d0a689700692f4708b6e5b7
Mark Anthony Llego <111579522+llegomark@users.noreply.github.com>
2023-01-05 15:30:34 +0800

    Updates

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,10 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-// import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
-// Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { prisma } from "../../../server/db";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
